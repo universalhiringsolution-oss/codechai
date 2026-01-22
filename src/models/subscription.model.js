d56b846efc1e68b674d1b0009a322c95327469fd
Your Name <your.email@example.com>
2026-01-22 07:19:28 +0530

    add subscribers

---
+++ src/models/subscription.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId,  // One is subscribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // One to whom ' subscriber '   is subscribing
+        ref: 'User'
+    }
+}, { timestamps: true })
+
+
+export const Subscription = mongoose.model("Subscription",
+    subscriptionSchema)
+
+
+
+
+
+
+
